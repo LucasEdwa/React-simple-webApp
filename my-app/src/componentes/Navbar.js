4161e1614f6ac7ab5ccb7cc8a69635cfd5b8f89f
LucasEdwa <117824594+LucasEdwa@users.noreply.github.com>
2023-11-05 09:26:30 +0100

    Update Navbar.js

--- my-app/src/componentes/Navbar.js
+++ my-app/src/componentes/Navbar.js
@@ -1,43 +1,55 @@
-import { Link } from "react-router-dom";
-import { useContext } from "react";
-import { AuthContext } from "../contexts/AuthContext";
+import item01 from '../img/banner-1.jpg';
+import item02 from '../img/banner-10.jpg';
+import item03 from '../img/background.jpg';
+import ButtonForMore from './ButtonForMore';
 
-
-
-function Navbar() {
-  const [auth, setAuth] = useContext(AuthContext);
-  return (
-    <nav className="flex bg-slate-900 items-center fixed w-full z-50 ">
+const products = [
+    {
+        id: 1,
+        name: 'eCommerce webpage (Simple)',
+        description: 'eCommerce webpage with main page, cart-bar and products.',
+        image: item01,
         
-        <h1 className="p-5 text-amber-500">HiStore</h1>
+      },
+      {
+        
+        id: 2,
+        name: 'eCommerce webpage with contact form',
+        description: 'eCommerce webpage using TailwindCSS and React with home page, products page, contact form page and cart bar.',
+        image: item02,
+      },
+      {
+        
+        id: 3,
+        name: 'eCommerce webpage ',
+        description: 'eCommerce webpage using TailwindCSS and React with home page, products page, contact form page, slides and animations and cart bar.',
+        image: item03,
+      }
+]
 
-          <ul className='flex text-white p-5 gap-4'>
-
-            <NavItem title='Home' to='/' />
-            <NavItem title='Products' to='/products' />
-            <NavItem title='About us'  to='/about'/>
-            <NavItem title='Contact'  to='/contact '/>
-              
-            {auth ? 
-            //If auth is true then show the page otherwise null
-            <NavItem title='User Page'  to='/protect'/> : null}
-          </ul>
-      
-    </nav>
-  )
-}
-function NavItem({title, to}) {
+function ProductsList(){
     return(
-        <li className="hover:underline">
-          {/* Link is used instead of <a> and to is to connect
-          with the page link*/}
-          <Link to={to}>
-              {title}
-          </Link>
-           </li>
+        <>
 
+            <div className='product-container  flex space-x-10 m-10 border border-gray-500 p-9'>
+
+             {/* Mapping over products array to create product cards */}
+                {products.map((product) => (
+                    <div className='product-holder hover:shadow-custom-color border border-gray-500 p-8 w-full ' key={product.id}>
+                    <div className='product text-white'>
+                        <img className='w-full h-50' src={product.image} alt='product' />
+                        <h2 className='p-2 mb-1'>{product.name}</h2>
+                        <p>{product.description}
+                            {/* ButtonForMore component with onClick event */}
+                        
+                        </p>
+                        <ButtonForMore className='pt-5 pl-2' text= 'more' to='model1'/>
+                    </div>
+                    </div> 
+                ))} 
+            </div>
+        </>
     )
-
-
 }
-export { Navbar} ;
+
+export { ProductsList };
