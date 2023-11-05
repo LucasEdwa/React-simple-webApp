943b577596f5b7e4e3caf20aa8fce88d93f7095c
LucasEdwa <117824594+LucasEdwa@users.noreply.github.com>
2023-11-05 09:30:25 +0100

    Update Home.js

--- my-app/src/pages/Home.js
+++ my-app/src/pages/Home.js
@@ -5,7 +5,6 @@ import {slider, settings } from '../componentes/Slider';
 import '../index.css'
 
 import {Header} from '../componentes/Header';
-import ButtonForMore from '../componentes/ButtonForMore';
 import { ProductsList } from '../componentes/ProductsList';
 
 
@@ -40,23 +39,21 @@ function Home(){
     return(
         <>
           {/* Header component */}
-          <Header title='Home' />
+          <Header className='' title='Home' />
           {/* Slider component */}
-          <div className='banner m-16'>
-            <Slider className='overflow-x-hidden overflow-y-hidden' {...settings}>
+          <div className='banner m-10'>
+            <Slider className='overflow-hidden' {...settings}>
               {/* Mapping over banners array to create slides */}
 
               {slider.map((slider, index) => (
                 // Adding slider.color to the className to change the background color
                 <div key={index} className={`${slider.color} text-white `}>                  
                 {/* Adding slider.src to the src to change the image */}
-                <img className='w-full ' src={slider.src} alt='banner' />
-                  <div className=' p-5'>
+                <img className='w-full object-cover h-40 overflow-y-hidden' src={slider.src} alt='banner' />
+                  <div className=' p-10'>
                     <h1 className='text-4xl font-bold '>{slider.h1}</h1>
                     <p >{slider.p}</p>
-                    {/* ButtonForMore component with onClick event */}
 
-                    <ButtonForMore className='hover:underline' text={slider.button} onClick={() => alert('More info about this product')} />
                   </div>
                 </div>
 
@@ -67,7 +64,7 @@ function Home(){
           </div>
            <main>
            {/* Section for displaying features */}
-           <div className='flex  bg-gray-600 p-8 space-x-40 justify-center text-amber-400'>
+           <div className='flex  bg-red-900 p-8 space-x-40 justify-center text-amber-400'>
              <h1>Best upgrade solutions</h1>
              <h1>Good Prices</h1>
              <h1>High quality webDesign</h1>
@@ -83,7 +80,7 @@ function Home(){
             <div className="flex flex-col gap-4 m-16 text-white">
                 {/* If error then show the error message otherwise null */}
                 {error?             
-                <p className="bg-red-600 text-white p-4 w-1/4 rounded-xl">You entered the worng password. Please, try again.</p>
+                <p className="bg-red-600 p-4 w-1/4 rounded-xl">You entered the worng password. Please, try again.</p>
                 :null}
 
                 <h1 className="text-4xl font-semibold" > Want see more? You are currently {auth ? 'signed in' : 'signed out'}</h1>
